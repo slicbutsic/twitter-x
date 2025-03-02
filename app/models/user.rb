aec0fadbcc4f6c7c863b5b62aa73af49bad683b9
Lucas Siviglia <siviglialucas@gmail.com>
2025-03-03 09:57:56 +1000

    associations for comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
