62971911d9a79288244057d422fd2251eb2ece03
Lucas Siviglia <siviglialucas@gmail.com>
2025-03-08 17:01:16 +1000

    add edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:bio)
+    params.require(:user).permit(:first_name, :last_name, :bio)
   end
 end
