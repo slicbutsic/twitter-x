e56294d8825c34da1b9520cad3dead69b5d5ed7b
Lucas Siviglia <siviglialucas@gmail.com>
2025-03-10 13:58:19 +1000

    add simple pagination

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ gem "tailwindcss-ruby", "~> 4.0"
 gem "tailwindcss-rails", "~> 4.1"
 
 gem 'faker'
+
+gem 'kaminari'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
   def index
     # @posts = Post.all
-    @posts = Post.from_followed_users(current_user).order(created_at: :desc)
+    @posts = Post.from_followed_users(current_user).order(created_at: :desc).page(params[:page]).per(15)
   end
 
   def show
